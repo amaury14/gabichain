cf874a8d8b273f12d86809c215aed8ae01e15e40
Amaury Chong <achong@carrasco.uruit.com>
2021-10-16 14:17:42 -0300

    added bodyparser and post endpoint

--- src/lib/app/index.js
+++ src/lib/app/index.js
@@ -1,13 +1,23 @@
  const express = require('express');
+ const bodyParser = require('body-parser');
  const GBlockChain = require('../blockchain');
+ 
  const HTTP_PORT = process.env.HTTP_PORT || 3000;
  const app = express();
  const bc = new GBlockChain();
+ app.use(bodyParser.json())
 
  app.get('/blocks', (req, res) => {
     res.json(bc.gchain);
  });
 
+ app.post('/mine', (req, res) => {
+     console.log(req.body);
+     const block = bc.addBlock(req.body.data);
+     console.log(`New block added: ${block.toString()}`);
+     res.redirect('/blocks');
+ })
+
  app.listen(HTTP_PORT, () => {
-     console.log('GBlockchain listening on port: ', HTTP_PORT);
+     console.log('GBlockChain listening on port: ', HTTP_PORT);
  });
